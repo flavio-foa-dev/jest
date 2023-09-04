4437bb2a80ee78a372769565077de1d87817a932
Flavio Andrade <foa12381@gmail.com>
2023-09-04 11:14:05 -0300

    add eslint in project

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
+const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
 
-const som =(a,b)=> a+b
+const calculaDescontos = (salario, descontos) => salario - descontos;
+
+const teste = (titulo, esperado, retornado) => {
+  if (esperado === retornado) {
+    console.log(`${titulo} passou`);
+  } else {
+    console.error(`${titulo} não passou`);
+  }
+};
+
+teste('somaHorasExtras', 2500, somaHorasExtras(2000, 500));
+
+teste('calculaDesconto', 2200, calculaDescontos(2500, 300));
